04e913a3ee6bdbb5eb0ce2fda590de06c48f8222
0krasimira <kstoyanova2@gmail.com>
2024-02-15 12:17:47 +0200

    add static register page

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express()
 const router = require("./controllers/homeController")
 // const movieRouter = require("./controllers/movieController")
 // const castRouter = require("./controllers/castController")
-// const authRouter = require("./controllers/authController")
+const userController = require("./controllers/userController")
 
 
 
@@ -21,7 +21,7 @@ const PORT = 3000
 app.use(router)
 // app.use(movieRouter)
 // app.use("/cast", castRouter)
-// app.use('/auth', authRouter)
+app.use('/auth', userController)
 
 mongoose.connect("mongodb://127.0.0.1:27017/secondHand-electronics").then(() => {console.log("DB connected succesfully.");
 app.listen(PORT, () => 
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,12 @@ const router = require('express').Router()
 const homeController = require('./controllers/homeController')
 // const electronicController = require('./controllers/electronicController')
 // const castController = require("./controllers/castController")
-// const authController = require('./controllers/authController')
+const authController = require('./controllers/authController')
 
 router.use(homeController)
 // router.use(electronicController)
 // router.use('/cast', castController)
-// router.use('/auth', authController)
+router.use('/auth', authController)
 
 router.get('*', (req,res) => {
     res.redirect('/404')
