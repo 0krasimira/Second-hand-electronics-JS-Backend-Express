2dfff5a2fa3fa8e66330ce109e22995b8352c0cb
0krasimira <kstoyanova2@gmail.com>
2024-02-15 16:10:32 +0200

    add is owner functionality

--- src/controllers/electronicsController.js
+++ src/controllers/electronicsController.js
@@ -38,11 +38,16 @@ router.get('/:electronicId/details', async(req, res) => {
     const electronicId = req.params.electronicId
     try{
         const electronic = await electronicManager.getOneWithDetails(electronicId).lean()
-        const isOwner = electronic.owner && electronic.owner == req.user?._id//movie.owner(object) ==  req.user._id(string) (convirts them to the same type)
+ 
+        const one = electronic.owner._id.toString()
+        const two =  electronic.owner == req.user?._id.toString()
+        const isOwner = electronic.owner._id.toString() == req.user?._id.toString()//movie.owner(object) ==  req.user._id(string) (convirts them to the same type)
         // const casts = await castManager.getByIds(movie.casts).lean() //--- only if populate is not used(populates the cast info into the movie with the ref: Cast in the Movie Schema)
+ 
+        console.log(isOwner)
         res.render('electronic/details', {electronic, isOwner})
 } catch(error){
-    res.status(404).redirect('404')
+    res.status(400).redirect('/404')
 }
  
 })
